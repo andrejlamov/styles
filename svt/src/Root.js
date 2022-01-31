7c64096282b64fa591913eeddd1e66bf50a3520f
Andrej Lamov <andrej.lamov@gmail.com>
2022-01-31 09:34:55 +0100

    Set right label with idx logic - remove utils

--- svt/src/Root.js
+++ svt/src/Root.js
@@ -1,6 +1,5 @@
 import { ChevronDown, ChevronUp } from "./components/icons";
 import clsx from "clsx";
-import { surroundEach } from "./utils";
 import { useState } from "react";
 
 export default function Root() {
@@ -11,7 +10,9 @@ export default function Root() {
           <NavigationBar />
         </header>
         <main className="h-full ">
-          <div className="h-full bg-neutral-100 flex justify-center">hej hej hej</div>
+          <div className="h-full bg-neutral-100 flex justify-center">
+            hej hej hej
+          </div>
         </main>
         <footer></footer>
       </div>
@@ -37,57 +38,58 @@ function NavigationBar() {
         <span className="font-extralight text-3xl">NYHETER</span>
       </a>
       <span className="pr-5"></span>
-      {surroundEach(
-        data.header_links.map(({ label, menu }) => {
-          const isActive = label === rootLabel && showSubMenu;
-          return (
-            <span className="h-full relative">
-              <a
-                className="h-full "
-                className={clsx(
-                  "  self-stretch px-3 h-full flex flex-row relative justify-center items-center hover:bg-neutral-50",
-                  { "bg-neutral-50": isActive }
-                )}
-                key={label}
-                href={`#${label}`}
-                onClick={(e) => {
-                  setShowSubMenu(null);
-                  setRootLabel(label);
-                }}
-              >
-                <span className="font-semibold text-lg">{label}</span>
-                {menu && (
-                  <button
-                    onClick={(e) => {
-                      if (label === rootLabel && !showSubMenu) {
-                        setShowSubMenu(menu);
-                        setRootLabel(label);
-                      } else if (label == rootLabel) {
-                        setShowSubMenu(null);
-                        setRootLabel(null);
-                      } else {
-                        setShowSubMenu(menu);
-                        setRootLabel(label);
-                      }
-                      e.stopPropagation();
-                    }}
-                    className={clsx(
-                      " rounded-full ml-3 relative",
-                      isActive
-                        ? "bg-red-600 text-white hover:bg-red-700"
-                        : "bg-neutral-200 text-neutral-500 hover:bg-neutral-300"
-                    )}
-                  >
-                    {isActive ? <ChevronUp className="h-5 w-5" /> : <ChevronDown className="h-5 w-5" />}
-                  </button>
-                )}
-              </a>
-              {showSubMenu && rootLabel === label && <LinkSubMenu />}
-            </span>
-          );
-        }),
-        <span className=" border-neutral-300 self-stretch h-full border-r-[1px]"></span>
-      )}
+      {data.header_links.map(({ label, menu }, i) => {
+        const isActive = label === rootLabel && showSubMenu;
+        return (
+          <span className="h-full relative" key={label}>
+            <a
+              className={clsx(
+                "  self-stretch px-3 h-full flex flex-row relative justify-center items-center hover:bg-neutral-50",
+                { "bg-neutral-50": isActive },
+                "border-neutral-300 self-stretch h-full border-r-[1px]",
+                { "border-l-[1px]": i == 0 }
+              )}
+              href={`#${label}`}
+              onClick={(e) => {
+                setShowSubMenu(null);
+                setRootLabel(label);
+              }}
+            >
+              <span className="font-semibold text-lg">{label}</span>
+              {menu && (
+                <button
+                  onClick={(e) => {
+                    if (label === rootLabel && !showSubMenu) {
+                      setShowSubMenu(menu);
+                      setRootLabel(label);
+                    } else if (label == rootLabel) {
+                      setShowSubMenu(null);
+                      setRootLabel(null);
+                    } else {
+                      setShowSubMenu(menu);
+                      setRootLabel(label);
+                    }
+                    e.stopPropagation();
+                  }}
+                  className={clsx(
+                    " rounded-full ml-3 relative",
+                    isActive
+                      ? "bg-red-600 text-white hover:bg-red-700"
+                      : "bg-neutral-200 text-neutral-500 hover:bg-neutral-300"
+                  )}
+                >
+                  {isActive ? (
+                    <ChevronUp className="h-5 w-5" />
+                  ) : (
+                    <ChevronDown className="h-5 w-5" />
+                  )}
+                </button>
+              )}
+            </a>
+            {showSubMenu && rootLabel === label && <LinkSubMenu />}
+          </span>
+        );
+      })}
     </nav>
   );
 }
@@ -97,7 +99,7 @@ function LinkSubMenu({ className }) {
     <div
       className={clsx(
         " bg-white shadow-sm font-light  absolute w-[512px] left-[calc(100%-25px-256px)] top-[calc(theme(space.20))] ",
-        "before:content('') before:absolure before:border-[7px] before:absolute before:border-t-transparent before:border-r-transparent before:border-b-red-600 before:border-l-transparent before:left-[calc(50%-3px)] before:top-[-18px]",
+        "before:content('') before:absolure before:border-[7px] before:absolute before:border-t-transparent before:border-r-transparent before:border-b-red-600 before:border-l-transparent before:left-[calc(50%-5px)] before:top-[-16px]",
         className
       )}
     >
--- svt/src/utils.js
+++
@@ -1,7 +0,0 @@
-export function intersperse(arr, sep) {
-  return arr.reduce((a, v) => [...a, v, sep], []).slice(0, -1);
-}
-
-export function surroundEach(arr, sep) {
-  return [sep, ...intersperse(arr, sep), sep];
-}
